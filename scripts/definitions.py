98a2d1bb24ca0bec50738e96e6fd3bc1d2f1fa8b
Aridai Bordón <aridaybordon@gmail.com>
2022-02-19 23:04:09 +0000

    fix: add flexibility to lorentz_boost

--- scripts/definitions.py
+++ scripts/definitions.py
@@ -2,15 +2,17 @@ import numpy as np
 
 c = 1     # Light velocity
 
-def beta(v_rel):
+def beta(v_rel) -> float:
     return v_rel/c
 
 
-def gamma(v_rel):
+def gamma(v_rel) -> float:
     return 1 / np.sqrt(1 - beta(v_rel)**2)
 
 
-def lorentz_boost(coords, v_rel):
+def lorentz_boost(coords, v_rel) -> list:
+    coords = np.matrix([[x] for x in coords]) if type(coords) == list else coords
+    
     b = beta(v_rel)
-    trans = gamma(v_rel) * np.matrix([[1, -b], [-b, 1]])
-    return trans.dot(coords)
+    trans = gamma(v_rel) * np.matrix([[1, -b], [-b, 1]])    
+    return [float(x) for x in trans.dot(coords)]
